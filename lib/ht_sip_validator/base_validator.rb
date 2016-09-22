71d73eadc180eb23c3dfe1b4ff30341a565ee5bc
Aaron Elkiss <aelkiss@umich.edu>
2016-09-22 15:30:31 -0400

    Validator for meta.yml presence

--- lib/ht_sip_validator/base_validator.rb
+++ lib/ht_sip_validator/base_validator.rb
@@ -1,19 +1,28 @@
 module HathiTrust
   class BaseValidator
+    attr_accessor :errors
+    attr_accessor :warnings
+
     def initialize(sip)
       @sip = sip
+      @errors = []
+      @warnings = []
     end
 
     def valid?
       true
     end
 
-    def errors
-      []
+    private
+      
+    def record_and_return_error_message(message)
+      @errors.push(message)
+      false
     end
 
-    def warnings
-      []
+    def record_warning_message(message)
+      @warnings.push(message)
+      true
     end
   end
 end
---
+++ lib/ht_sip_validator/meta_yml_validators.rb
@@ -0,0 +1,12 @@
+require 'ht_sip_validator/base_validator'
+
+module HathiTrust
+  module MetaYmlValidators
+    class ExistsValidator < BaseValidator
+      def valid?
+        @sip.files.include?("meta.yml") or 
+          record_and_return_error_message("Package is missing meta.yml")
+      end
+    end
+  end
+end
