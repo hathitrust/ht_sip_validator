198ec98eaf19d125b0e0b05ec5b1235343ca58ca
Aaron Elkiss <aelkiss@umich.edu>
2016-10-03 15:13:24 -0400

    spec for required meta yml keys

--- spec/validation/meta_yml_spec.rb
+++ spec/validation/meta_yml_spec.rb
@@ -17,9 +17,9 @@ module HathiTrust
     end
 
     module MetaYml
-
       describe Exists do
         let(:mocked_sip) { SIP::SIP.new("") }
+
         subject(:validator) { described_class.new(mocked_sip) }
 
         describe "#validate" do
@@ -61,8 +61,40 @@ module HathiTrust
             end
 
             it "has underlying details of the problem" do
-              expect( validator.validate.map(&:root_cause) )
+              expect(validator.validate.map(&:root_cause))
                 .to include(a_string_matching(/ tab /))
+            end
+          end
+        end
+      end
+
+      describe RequiredKeys do
+        def valid_yaml
+          YAML.load("capture_date: 2013-11-01T12:31:00-05:00")
+        end
+
+        def invalid_yaml
+          YAML.load("capture_elephant: do it.")
+        end
+
+        describe "#validate" do
+          let(:mocked_sip) { SIP::SIP.new("") }
+          subject(:validator) { described_class.new(mocked_sip) }
+
+          context "when meta.yml has capture_date" do
+            before(:each) { allow(mocked_sip).to receive(:meta_yml) .and_return(valid_yaml) }
+
+            it_behaves_like "a validator with a valid package"
+          end
+
+          context "when meta.yml does not have capture_date" do
+            before(:each) { allow(mocked_sip).to receive(:meta_yml) .and_return(invalid_yaml) }
+
+            it_behaves_like "a validator with an invalid package"
+
+            it "returns an appropriate error" do
+              expect(human_messages(validator.validate))
+                .to include(a_string_matching(/capture_date/))
             end
           end
         end
