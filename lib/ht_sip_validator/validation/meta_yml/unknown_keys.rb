fe0eb14e765d4ab257fd6e37c8c25b8fac8d0689
Bryan Hockey <bhock@umich.edu>
2016-10-13 17:44:58 -0500

    use new Validation::Base in subclasses

--- lib/ht_sip_validator/validation/meta_yml/unknown_keys.rb
+++ lib/ht_sip_validator/validation/meta_yml/unknown_keys.rb
@@ -12,17 +12,15 @@ module HathiTrust
                         image_compression_agent image_compression_tool scanning_order
                         reading_order pagedata).to_set
 
-        def validate
-          @sip.meta_yml.keys.to_set.difference(KNOWN_KEYS).each do |key|
-            record_warning(
+        def perform_validation
+          @sip.meta_yml.keys.to_set.difference(KNOWN_KEYS).map do |key|
+            create_warning(
               validation: :field_valid,
               human_message: "Unknown key #{key} in meta.yml",
               extras: { filename: "meta.yml",
                         field: key }
             )
           end
-
-          super
         end
       end
     end
