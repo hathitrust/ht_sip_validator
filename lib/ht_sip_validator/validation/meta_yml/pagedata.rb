6b0bd1c80123eedfa15442650e4ed32444315eb9
Aaron Elkiss <aelkiss@umich.edu>
2016-10-13 12:42:04 -0400

    validate allowed page tags

--- lib/ht_sip_validator/validation/meta_yml/pagedata.rb
+++ lib/ht_sip_validator/validation/meta_yml/pagedata.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
-require "ht_sip_validator/validation/base"
-
 module HathiTrust::Validation::MetaYml::PageData
 end
 
@@ -8,3 +6,4 @@ require "ht_sip_validator/validation/meta_yml/page_data/presence"
 require "ht_sip_validator/validation/meta_yml/page_data/keys"
 require "ht_sip_validator/validation/meta_yml/page_data/values"
 require "ht_sip_validator/validation/meta_yml/page_data/files"
+require "ht_sip_validator/validation/meta_yml/page_data/page_tags"
