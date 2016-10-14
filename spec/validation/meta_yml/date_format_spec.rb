431ad98bc54fa182de84d3ae19c4e7715ea7b879
Bryan Hockey <bhock@umich.edu>
2016-10-14 15:21:36 -0500

    date_format_spec defers to messages.yml

--- spec/validation/meta_yml/date_format_spec.rb
+++ spec/validation/meta_yml/date_format_spec.rb
@@ -13,6 +13,20 @@ module HathiTrust
         let(:sip) { double(:sip, meta_yml: meta_yml) }
         subject(:validator) { described_class.new(double(:sip)) }
 
+        let(:capture_date_error) do
+          message_for(
+            described_class.to_s,
+            :capture_date,
+            {filename: "meta.yml", actual: capture_date })
+        end
+
+        let(:image_compression_date_error) do
+          message_for(
+            described_class.to_s,
+            :image_compression_date,
+            {filename: "meta.yml", actual: image_compression_date })
+        end
+
         context "when iso8601" do
           let(:capture_date) { "2016-12-08T01:02:03-05:00" }
           let(:image_compression_date) { "2000-02-29T12:12:59Z" }
@@ -24,55 +38,27 @@ module HathiTrust
         context "when missing" do
           let(:capture_date) { "2016-12-08T01:02:03-05:00" }
           let(:image_compression_date) { nil }
-          let(:errors) do
-            [validator.create_error(
-              validation: :image_compression_date,
-              human_readable: "An iso8601 combined date is required for image_compression_date in meta.yml.",
-              extras: {filename: "meta.yml", actual: image_compression_date }
-            )]
-          end
           it "returns the errors" do
-            expect(validator.validate).to eql(errors)
+            expect(validator.validate)
+              .to contain_exactly(image_compression_date_error)
           end
         end
 
         context "when wrong format" do
           let(:capture_date) { "2007-11-19 08:25:02 -0600" }
           let(:image_compression_date) { "2010-03-30T05:43:25.1235000000Z" }
-          let(:errors) do
-            [
-              validator.create_error(
-                validation: "capture_date",
-                human_readable: "An iso8601 combined date is required for capture_date in meta.yml.",
-                extras: {filename: "meta.yml", actual: capture_date }),
-              validator.create_error(
-                validation: "image_compression_date",
-                human_readable: "An iso8601 combined date is required for image_compression_date in meta.yml.",
-                extras: {filename: "meta.yml", actual: image_compression_date })
-            ]
-          end
           it "returns the errors" do
-            expect(validator.validate).to eql(errors)
+            expect(validator.validate)
+              .to contain_exactly(capture_date_error, image_compression_date_error)
           end
         end
 
         context "when impossible date" do
           let(:capture_date) { "2001-02-29T01:02:03-03:00" }
           let(:image_compression_date) { "2002-02-29T12:12:59Z" }
-          let(:errors) do
-            [
-              validator.create_error(
-                validation: "capture_date",
-                human_readable: "The supplied date for capture_date in meta.yml is impossible.",
-                extras: {filename: "meta.yml", actual: capture_date }),
-              validator.create_error(
-                validation: "image_compression_date",
-                human_readable: "The supplied date for image_compression_date in meta.yml is impossible.",
-                extras: {filename: "meta.yml", actual: image_compression_date })
-            ]
-          end
           it "returns the errors" do
-            expect(validator.validate).to eql(errors)
+            expect(validator.validate)
+              .to contain_exactly(capture_date_error, image_compression_date_error)
           end
         end
 
