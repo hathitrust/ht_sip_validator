a6f210891e517ad047f2c1a1613d175f71138a9c
Colin Gross <grosscol@gmail.com>
2016-10-27 11:29:51 -0400

    Update invalid package shared examples   Fixes bug where an empty array would satisy the examples.   Add spec to require at least one Messages.   Add spec to require at least one error Message.

--- spec/support/examples/a_validation_with_an_invalid_package.rb
+++ spec/support/examples/a_validation_with_an_invalid_package.rb
@@ -2,7 +2,15 @@
 # frozen_string_literal: true
 # let(:validation) { SomeValidator.new }
 shared_examples_for "a validation with an invalid package" do
+  it "returns at least one Message" do
+    expect(validation.validate).not_to be_empty
+  end
+
   it "returns a collection of Messages" do
     expect(validation.validate).to all(be_an_instance_of(HathiTrust::Validation::Message))
+  end
+
+  it "return errors" do
+    expect(any_errors?(validation.validate)).to be true
   end
 end
