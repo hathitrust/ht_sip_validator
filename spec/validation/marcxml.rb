c0afce51125b070b4db7b5454ba0baa62cd246c5
Aaron Elkiss <aelkiss@umich.edu>
2016-11-16 09:34:05 -0500

    Warn if marc.xml is present
     Closes #15

---
+++ spec/validation/marcxml.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+require "spec_helper"
+
+module HathiTrust
+  describe Validator::MarcXML do
+    let(:mocked_sip) { SIP::SIP.new("") }
+    let(:validator) { described_class.new(mocked_sip) }
+
+    describe "#validate" do
+      context "when package does not contain marc.xml" do
+        let(:file_list) do
+          %w(00000001.tif 00000001.txt checksum.md5 meta.yml)
+        end
+        before(:each) { allow(mocked_sip).to receive(:files).and_return(file_list) }
+
+        it_behaves_like "a validator with a valid package"
+        it_behaves_like "a validator that returns no messages"
+      end
+
+      context "when package contains marc.xml" do
+        let(:file_list) { %w(00000001.tif 00000001.txt checksum.md5 meta.yml marc.xml) }
+        before(:each) { allow(mocked_sip).to receive(:files).and_return(file_list) }
+
+        it_behaves_like "a validator with warnings and only warnings"
+
+        it "returns an appropriate message" do
+          expect(human_messages(validator.validate))
+            .to include(a_string_matching(/marc.xml/))
+        end
+      end
+    end
+  end
+end
