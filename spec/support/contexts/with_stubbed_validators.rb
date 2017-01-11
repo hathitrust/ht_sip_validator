290c7450dd048eeb743429fbe573e1a4d0450d56
Bryan Hockey <bhock@umich.edu>
2017-01-11 14:51:23 -0600

    Log errors and warnings at that level.

--- spec/support/contexts/with_stubbed_validators.rb
+++ spec/support/contexts/with_stubbed_validators.rb
@@ -2,7 +2,13 @@
 require "spec_helper"
 
 shared_context "with stubbed validators" do
-  let(:message) { double("a validator message", to_s: "uno\ndos", error?: false) }
+  let(:message) do
+    double("a validator message",
+      to_s: "uno\ndos",
+      error?: false,
+      warning?: false
+    )
+  end
   let(:validator_instance)  { double("a validator", validate: [message]) }
 
   before(:each) do
