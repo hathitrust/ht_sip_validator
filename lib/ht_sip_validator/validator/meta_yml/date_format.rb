94cbee743b325426e1912caf64c5100e5378f78f
Aaron Elkiss <aelkiss@umich.edu>
2019-05-01 16:41:09 -0400

    HTREPO-214: Validate date against regex
    Testing with strptime has some extra benefits as it will also complain
    if any of the date fields are out of range. However, strptime is more
    accepting of variant formatting (i.e. non-zero-padded fields) than the
    regex is.

--- lib/ht_sip_validator/validator/meta_yml/date_format.rb
+++ lib/ht_sip_validator/validator/meta_yml/date_format.rb
@@ -12,21 +12,24 @@ module HathiTrust::Validator
 
     def perform_validation
       messages = []
+
       FIELDS.each do |field|
+        next if sip.metadata[field].nil?
+        had_error = false
+
+        # regex from edtfRegularExpressions in https://www.loc.gov/standards/premis/v2/premis-v2-3.xsd
+        if !sip.metadata[field].match(%r{\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}((Z|(\+|-)\d{2}:\d{2}))?})
+          had_error = true
+        end
+
         begin
-          unless sip.metadata[field].nil?
-            DateTime.strptime(sip.metadata[field], DATE_FORMAT)
-          end
+          DateTime.strptime(sip.metadata[field], DATE_FORMAT)
         rescue ArgumentError
-          messages << create_error(
-            validation_type: field.to_sym,
-            human_message: human_message(field),
-            extras: {
-              filename: "meta.yml",
-              field: field,
-              actual: sip.metadata[field]
-            }
-          )
+          had_error = true
+        end
+
+        if had_error
+          messages << error_for(field)
         end
       end
       return messages
@@ -34,6 +37,20 @@ module HathiTrust::Validator
 
     def human_message(field)
       "An iso8601 combined date (e.g 2016-12-08T01:02:03-05:00) is required for #{field} in meta.yml."
+    end
+
+    private
+
+    def error_for(field)
+      create_error(
+        validation_type: field.to_sym,
+        human_message: human_message(field),
+        extras: {
+          filename: "meta.yml",
+          field: field,
+          actual: sip.metadata[field]
+        }
+      )
     end
 
   end
